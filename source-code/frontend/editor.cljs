e04c0abff3d045d82b66a296e50befe34c45cbfd
Paul Cristian <paul931224@gmail.com>
2023-07-04 06:08:36 +0200

    Added some textures.

--- source-code/frontend/editor.cljs
+++ source-code/frontend/editor.cljs
@@ -13,20 +13,26 @@
                  (/ (.-innerWidth js/window)
                     (.-innerHeight js/window))
                  0.1
-                 1000))
+                 2000))
 
-(.setZ (-> camera .-position) 30)
+(.setZ (-> camera .-position) 15)
 
 
-(def renderer (new three/WebGLRenderer))
+(def renderer (new three/WebGLRenderer
+                   #js {:antialias true
+                        :alpha true}))
 
 (.setPixelRatio renderer (.-devicePixelRatio js/window))
 (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
+(.setClearColor renderer 0xffffff 0)
 
 (defn add-cube! [path]
   (let [scene @(subscribe [:db/get [:scene]])
         geometry (new three/BoxGeometry 2 1 1)
-        material (new three/MeshStandardMaterial #js {:color 0x00ff00})
+        texture  (.load
+                  (new three/TextureLoader)
+                  "/images/texture.jpg")
+        material (new three/MeshStandardMaterial #js {:map texture})
         cube     (new three/Mesh geometry material)]
     (.add scene cube)
 
@@ -34,9 +40,9 @@
 
 (defn add-light! [path]
   (let [scene @(subscribe [:db/get [:scene]])
-        light (new three/PointLight 0xffffff)
+        light (new three/PointLight 0xffffff 1)
         light-helper (new three/PointLightHelper light)]
-    (.set (.-position light) 5 5 5)
+    (.set (.-position light) 3 3 3)
     (.add scene light light-helper)
     (dispatch-sync [:db/set! path light])))
 
@@ -53,7 +59,12 @@
    (.-domElement renderer)))
 
 (defn add-scene-to-db! []
-  (dispatch-sync [:db/set! [:scene] (new three/Scene)]))
+  (let [scene (new three/Scene)
+        space-texture (.load
+                       (new three/TextureLoader)
+                       "/images/background.jpg")]
+    (set! (.-background scene) space-texture)
+    (dispatch-sync [:db/set! [:scene] scene])))
 
 (add-scene-to-db!)
 
